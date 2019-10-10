e69ddccb7921ce4fe519b6cb7b081fb302b913e5
Alex Moreno <alex.moreno.costa@gmail.com>
2019-10-09 21:36:46 -0300

    Trabalhando mais com lambdas

---
+++ src/main/java/br/com/amcosta/alurajavautil/ExercicioLambdas.java
@@ -0,0 +1,23 @@
+package br.com.amcosta.alurajavautil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ExercicioLambdas {
+
+    public static void main(String[] args) {
+
+        List<String> nomes = new ArrayList<>(Arrays.asList("Super Mario", "Yoshi", "Donkey Kong"));
+//        Collections.sort(nomes, new Comparator<String>() {
+//
+//            @Override
+//            public int compare(String s, String t1) {
+//                return s.compareTo(t1);
+//            }
+//        });
+
+        nomes.sort((n1, n2) -> n1.compareTo(n2));
+        nomes.forEach(nome -> System.out.println(String.format("Nintendo hero: %s", nome)));
+    }
+}
